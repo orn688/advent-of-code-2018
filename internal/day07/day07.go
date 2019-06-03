1965242edda8f969c7d2c1bdde14c123e385ffb7
Oliver Newman <orn688@gmail.com>
2019-06-02 19:01:28 -0700

    Refactor day 7 Part1 for easier reuse

--- internal/day07/day07.go
+++ internal/day07/day07.go
@@ -4,39 +4,156 @@ import (
 	"container/heap"
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/orn688/advent-of-code-2018/internal/util"
 )
 
-var lineRegex = regexp.MustCompile(`^Step (?P<Dependency>[A-Z]+) must be ` +
-	`finished before step (?P<Depender>[A-Z]+) can begin.$`)
+var lineRegex = regexp.MustCompile(`^Step (?P<Dependency>[A-Z]) must be ` +
+	`finished before step (?P<Depender>[A-Z]) can begin.$`)
 
+// A Requirement is a 2-tuple made up of a step name (the Depender) and the
+// name of a step that it depends on (the Dependency).
 type requirement struct {
 	// Dependency must be completed before Depender.
 	Depender   string
 	Dependency string
 }
 
-type dependencyGraph map[string][]string
+// A dependencyGraph is an adjacency list mapping step names to the names of
+// steps that depend on them.
+// ReverseAlphaOrder indicates whether a step named B should be built before a
+// step named A, all else being equal
+type dependencyGraph struct {
+	adjList                 map[string][]string
+	stepsReadyToBuild       util.StringHeap
+	unbuiltDependencyCounts map[string]int
+	reverseAlphaOrder       bool
+}
 
-// Part1 is unimplemented
+func newDependencyGraph(reqs []requirement, reverse bool) dependencyGraph {
+	graph := dependencyGraph{
+		adjList:                 make(map[string][]string),
+		stepsReadyToBuild:       make(util.StringHeap, 0),
+		unbuiltDependencyCounts: make(map[string]int, len(reqs)),
+		reverseAlphaOrder:       reverse,
+	}
+	for _, req := range reqs {
+		graph.adjList[req.Dependency] = append(
+			graph.adjList[req.Dependency], req.Depender,
+		)
+	}
+	graph.setDependencyCounts()
+	graph.setStepsReadyToBuild()
+	return graph
+}
+
+// Set the number of dependencies for each build step.
+func (graph *dependencyGraph) setDependencyCounts() {
+	for step := range graph.adjList {
+		graph.unbuiltDependencyCounts[step] = 0
+	}
+	for _, dependentSteps := range graph.adjList {
+		for _, step := range dependentSteps {
+			graph.unbuiltDependencyCounts[step]++
+		}
+	}
+}
+
+// Initialize the steps with no dependencies. This must be called after the
+// dependency counts have been initialized by calling setDependencyCounts.
+func (graph *dependencyGraph) setStepsReadyToBuild() {
+	for step, dependencyCount := range graph.unbuiltDependencyCounts {
+		if dependencyCount == 0 {
+			graph.markAsReadyToBuild(step)
+		}
+	}
+}
+
+func (graph *dependencyGraph) markAsReadyToBuild(step string) {
+	priority := int(step[0])
+	if graph.reverseAlphaOrder {
+		priority *= -1
+	}
+	heap.Push(&graph.stepsReadyToBuild, util.HeapElement{
+		Value:    step,
+		Priority: int(step[0]),
+	})
+}
+
+func (graph *dependencyGraph) stepWasBuilt(step string) {
+	for _, dependent := range graph.adjList[step] {
+		graph.unbuiltDependencyCounts[dependent]--
+		if graph.unbuiltDependencyCounts[dependent] == 0 {
+			graph.markAsReadyToBuild(dependent)
+		}
+	}
+}
+
+func (graph *dependencyGraph) getNextStep() (string, error) {
+	if len(graph.stepsReadyToBuild) == 0 {
+		return "", fmt.Errorf("no steps left to build")
+	}
+	element := heap.Pop(&graph.stepsReadyToBuild).(util.HeapElement)
+	return element.Value, nil
+}
+
+// Part1 returns a topological ordering of the steps, based on the requirements
+// in the input.
 func Part1(input string) (string, error) {
 	reqs, err := parseInput(input)
 	if err != nil {
 		return "", err
 	}
-	graph := buildGraph(reqs)
-	order, err := topoOrder(graph)
+	graph := newDependencyGraph(reqs, false)
+	ordering := make([]string, len(graph.adjList))
+
+	for i := 0; i < len(ordering); i++ {
+		nextStep, err := graph.getNextStep()
+		if err != nil {
+			// We got to a point where there are no uncompleted steps whose
+			// dependencies are all completed, but not all steps have been put
+			// into the ordering - i.e., a cycle.
+			return "", fmt.Errorf("cycle detected")
+		}
+		ordering[i] = nextStep
+		graph.stepWasBuilt(nextStep)
+	}
+
+	return strings.Join(ordering, ""), nil
+}
+
+// Part2 returns the time it would take workerCount workers to complete the
+// steps.
+func Part2(input string) (string, error) {
+	steps, err := Part1(input)
 	if err != nil {
 		return "", err
 	}
-	return strings.Join(order, ""), nil
-}
 
-// Part2 is unimplemented
-func Part2(input string) (string, error) {
-	return "", nil
+	workerCount := 5
+	currentJobDurations := make([]int, workerCount)
+	stepIndex := 0
+	complete := false
+	time := 0
+	for !complete {
+		complete = true // Assume, then verify
+		for i := range currentJobDurations {
+			if currentJobDurations[i] == 0 && stepIndex < len(steps) {
+				// TODO: we can't just take the next step; all of its
+				// dependencies must also be complete.
+				currentJobDurations[i] = stepDuration(steps[stepIndex])
+				stepIndex++
+			}
+			if currentJobDurations[i] > 0 {
+				currentJobDurations[i]--
+				complete = false
+			}
+		}
+		time++
+	}
+	return strconv.Itoa(time), nil
 }
 
 func parseInput(input string) ([]requirement, error) {
@@ -52,8 +169,8 @@ func parseInput(input string) ([]requirement, error) {
 	return reqs, nil
 }
 
-// Convert a line to a requirement, assuming each step name is a string of
-// capital ASCII letters.
+// Convert a line to a requirement, assuming each step name is a length-1
+// string of capital ASCII letters.
 func parseLine(line string) (req requirement, err error) {
 	groups, err := util.CaptureRegexGroups(lineRegex, line)
 	if err != nil {
@@ -66,61 +183,8 @@ func parseLine(line string) (req requirement, err error) {
 	return
 }
 
-func buildGraph(reqs []requirement) dependencyGraph {
-	graph := make(map[string][]string)
-	for _, req := range reqs {
-		graph[req.Dependency] = append(graph[req.Dependency], req.Depender)
-	}
-	return graph
-}
-
-func topoOrder(graph dependencyGraph) ([]string, error) {
-	ordering := make([]string, len(graph))
-
-	dependencyCounts := getDependencyCounts(graph)
-	stepsWithoutDependencies := getStepsWithoutDependencies(dependencyCounts)
-
-	for len(stepsWithoutDependencies) > 0 {
-		step := heap.Pop(&stepsWithoutDependencies).(string)
-		for _, dependent := range graph[step] {
-			dependencyCounts[dependent]--
-			if dependencyCounts[dependent] == 0 {
-				heap.Push(&stepsWithoutDependencies, dependent)
-			}
-		}
-		ordering = append(ordering, step)
-	}
-
-	if ordering[len(ordering)-1] == "" {
-		// We got to a point where there are no uncompleted steps whose
-		// dependencies are all completed, but not all steps have been put
-		// into the ordering - i.e., a cycle.
-		return []string{}, fmt.Errorf("cycle detected")
-	}
-	return ordering, nil
-}
-
-func getDependencyCounts(graph dependencyGraph) map[string]int {
-	dependencyCounts := make(map[string]int, len(graph))
-	for step := range graph {
-		dependencyCounts[step] = 0
-	}
-	for _, dependentSteps := range graph {
-		for _, step := range dependentSteps {
-			dependencyCounts[step]++
-		}
-	}
-	return dependencyCounts
-}
-
-func getStepsWithoutDependencies(depCounts map[string]int) util.StringHeap {
-	stepsWithoutDependencies := make(util.StringHeap, 0)
-	for step, dependencyCount := range depCounts {
-		if dependencyCount == 0 {
-			stepsWithoutDependencies.Push(step)
-		}
-	}
-	// Use a heap so that steps are ordered alphabetically if there's a "tie".
-	heap.Init(&stepsWithoutDependencies)
-	return stepsWithoutDependencies
+// Assumes the step has length 1 and is A-Z.
+func stepDuration(step byte) int {
+	durationOfStepA := 61
+	return durationOfStepA + (int(step) - int('A'))
 }
--- internal/util/priorityQueue.go
+++ internal/util/priorityQueue.go
@@ -1,14 +1,21 @@
 package util
 
-// A StringHeap implements heap.Stringerface and holds strings.
-type StringHeap []string
+// A StringHeap implements heap.Interface and holds strings.
+type StringHeap []HeapElement
+
+// A HeapElement assigns a priority to a string for storage and comparison in a
+// StringHeap.
+type HeapElement struct {
+	Value    string
+	Priority int
+}
 
 func (h StringHeap) Len() int {
 	return len(h)
 }
 
 func (h StringHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+	return h[i].Priority < h[j].Priority
 }
 
 func (h StringHeap) Swap(i, j int) {
@@ -17,9 +24,9 @@ func (h StringHeap) Swap(i, j int) {
 
 // Push a string onto the end of the heap.
 func (h *StringHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(string))
+	// Push and Pop use pointer receivers because they modify the slice's
+	// length, not just its contents.
+	*h = append(*h, x.(HeapElement))
 }
 
 // Pop a string from the top of the heap.
